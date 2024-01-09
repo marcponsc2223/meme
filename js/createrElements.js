cf955ce43350acf984f9b37bdb9a174881e2b72d
marcponsc2223 <123729056+marcponsc2223@users.noreply.github.com>
2024-01-09 18:02:18 +0100

    Comentarios Añadidos
    asdkasd

--- js/createrElements.js
+++ js/createrElements.js
@@ -1,25 +1,31 @@
+/** 1. Archivos de audio.*/
 var audioWeLive = document.getElementById('weLive')
 var fileWeLive = audioWeLive.getAttribute('src');
-var audio = new Audio(fileWeLive);   
+var audio = new Audio(fileWeLive);  
 
+/** 2. Crear y inicializar las variables del DOM.*/
 let container = document.getElementById('container')
-let smurfcat = document.createElement('img')
 let title = document.createElement('h1')
 title.classList.add('title')
 title.textContent = 'WE LIVE... WE LOVE... WE LIE...'
 title.style.display = 'none'
 container.appendChild(title)
+// 2.1 Smurfcat.
+let smurfcat = document.createElement('img')
 smurfcat.classList.add('smurfCat')
 smurfcat.src = '../img/smurfcat.png'
 smurfcat.style.display = 'none'
 container.appendChild(smurfcat)
+// 2.2 Forest.
 let img = 'url(../img/forest.jpg)'
 container.style.backgroundImage = img
+// 2.3 Botones.
 let button = document.createElement('span')
 button.classList.add('buttons')
 button.style.backgroundImage = 'url(../img/button.png)'
 container.appendChild(button)
 
+/** 3. Añadir los eventos.*/
 button.addEventListener('click', function() {
     container.style.animation = 'zoom 2s linear'
     button.style.display = 'none'
